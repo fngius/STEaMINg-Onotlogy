0ece7a982593572e487ab65f53e85c2c93669c8f
liorpeak <liorpeak@gmail.com>
2018-11-27 14:06:26 +0100

    first commit

--- src/main/java/lab/ssn/lab/ssn/ontology/App.java
+++ src/main/java/lab/ssn/lab/ssn/ontology/App.java
@@ -1,6 +1,11 @@
 package lab.ssn.lab.ssn.ontology;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Collection;
 
 import org.apache.jena.query.QueryExecution;
@@ -11,107 +16,233 @@ import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.update.UpdateAction;
 import org.apache.jena.vocabulary.RDFS;
+import org.apache.xerces.impl.dtd.models.DFAContentModel;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.io.OWLXMLOntologyFormat;
 import org.semanticweb.owlapi.model.AddImport;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAnnotationProperty;
+import org.semanticweb.owlapi.model.OWLAnnotationPropertyRangeAxiom;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLDatatype;
 import org.semanticweb.owlapi.model.OWLImportsDeclaration;
+import org.semanticweb.owlapi.model.OWLLiteral;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
 import org.semanticweb.owlapi.model.OWLObjectProperty;
+import org.semanticweb.owlapi.model.OWLObjectPropertyDomainAxiom;
+import org.semanticweb.owlapi.model.OWLObjectPropertyRangeAxiom;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyFormat;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+import org.semanticweb.owlapi.vocab.OWL2Datatype;
 
 import ru.avicomp.ontapi.OntManagers;
 import ru.avicomp.ontapi.OntologyManager;
 import ru.avicomp.ontapi.OntologyModel;
 
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws OWLOntologyCreationException
     {
         String uri = "http://ontology.com/test";
-        String ns = uri + "#";
+        //String ns = uri + "#";
 
-        OntologyManager m = OntManagers.createONT();
-        OWLDataFactory df = m.getOWLDataFactory();
+        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+        OWLDataFactory factory = manager.getOWLDataFactory();
+        
+        String ontologyURI = "http://industry.ont";
+        String ns = ontologyURI + "#";
+        
+        OWLOntology ontology = manager.createOntology(IRI.create(ontologyURI));
 
-        // compose ontology:
-        OntologyModel o = m.createOntology(IRI.create(uri));
-        OWLAnnotationProperty a1 = df.getOWLAnnotationProperty(IRI.create(ns, "prop1"));
-        OWLAnnotationProperty a2 = df.getOWLAnnotationProperty(IRI.create(ns, "prop2"));
-        OWLClass c1 = df.getOWLClass(IRI.create(ns, "class1"));
-        OWLNamedIndividual i1 = df.getOWLNamedIndividual(IRI.create(ns, "indi1"));
-        o.add(df.getOWLDeclarationAxiom(a1));
-        o.add(df.getOWLDeclarationAxiom(a2));
-        o.add(df.getOWLDeclarationAxiom(c1));
-        o.add(df.getOWLAnnotationPropertyDomainAxiom(a1, c1.getIRI()));
-        o.add(df.getOWLClassAssertionAxiom(c1, i1));
-        o.add(df.getOWLSubAnnotationPropertyOfAxiom(a2, a1));
-        o.add(df.getOWLAnnotationAssertionAxiom(a1, df.getOWLAnonymousIndividual(), i1.getIRI()));
+		OWLDatatype integerDatatype = factory.getIntegerOWLDatatype();
+		OWLDatatype floatDatatype = factory.getFloatOWLDatatype();
+		OWLDatatype doubleDatatype = factory.getDoubleOWLDatatype();
+		OWLDatatype booleanDatatype = factory.getBooleanOWLDatatype();
+	    OWLDatatype dateTime = factory.getOWLDatatype(IRI.create("http://www.w3.org/2001/XMLSchema#dateTime"));
 
         String pre_TIME = "http://www.w3.org/2006/time#";
-		OWLImportsDeclaration importDeclarationTIME = m.getOWLDataFactory().getOWLImportsDeclaration(IRI.create(pre_TIME));
-		m.applyChange(new AddImport(o, importDeclarationTIME));
-		
 		String pre_SOSAOnt = "http://www.w3.org/ns/sosa/";
 		String pre_SSNOnt = "http://www.w3.org/ns/ssn/";
-		OWLImportsDeclaration importDeclarationSSN = m.getOWLDataFactory().getOWLImportsDeclaration(IRI.create(pre_SSNOnt));
-		m.applyChange(new AddImport(o, importDeclarationSSN));
 
-		String sensor               = pre_SOSAOnt + "Sensor";
-		OWLClass Sensor             = df.getOWLClass(IRI.create(sensor));
-		OWLClass Property           = df.getOWLClass(IRI.create(pre_SSNOnt + "Property"));
-		OWLClass ObservableProperty = df.getOWLClass(IRI.create(pre_SOSAOnt + "ObservableProperty"));
-		OWLClass Observation        = df.getOWLClass(IRI.create(pre_SOSAOnt + "Observation"));
-		OWLClass Result             = df.getOWLClass(IRI.create(pre_SOSAOnt + "Result"));
+        OWLImportsDeclaration importDeclarationTIME = factory.getOWLImportsDeclaration(IRI.create(pre_TIME));
+		OWLImportsDeclaration importDeclarationSSN = factory.getOWLImportsDeclaration(IRI.create(pre_SSNOnt));
+		manager.applyChange(new AddImport(ontology, importDeclarationTIME));
+		manager.applyChange(new AddImport(ontology, importDeclarationSSN));
+
+		OWLClass Resource = factory.getOWLClass(IRI.create(ns,"Resource"));
 		
-		o.add(df.getOWLDeclarationAxiom(Sensor));
-		o.add(df.getOWLDeclarationAxiom(Property));
-		o.add(df.getOWLDeclarationAxiom(ObservableProperty));
-		o.add(df.getOWLDeclarationAxiom(Observation));
-		o.add(df.getOWLDeclarationAxiom(Result));
+		OWLClass Process = factory.getOWLClass(IRI.create(ns,"Process"));
 		
-		OWLObjectProperty observes = df.getOWLObjectProperty(IRI.create("http://www.w3.org/ns/sosa/observes"));
-		o.add(df.getOWLDeclarationAxiom(observes));
+		ontology.add(factory.getOWLDeclarationAxiom(Resource));
+		ontology.add(factory.getOWLDeclarationAxiom(Process));
 		
-		// Print ontology before updating:
-        //o.axioms().forEach(System.out::println);
+		OWLClass Platform 			= factory.getOWLClass(IRI.create(pre_SOSAOnt + "Platform"));
+		OWLClass System 			= factory.getOWLClass(IRI.create(pre_SOSAOnt + "System"));
+		OWLClass Sensor 			= factory.getOWLClass(IRI.create(pre_SOSAOnt + "Sensor"));
+		OWLClass Property 			= factory.getOWLClass(IRI.create(pre_SSNOnt  + "Property"));
+		OWLClass ObservableProperty = factory.getOWLClass(IRI.create(pre_SOSAOnt + "ObservableProperty"));
+		OWLClass Observation        = factory.getOWLClass(IRI.create(pre_SOSAOnt + "Observation"));
+		OWLClass FeatureOfInterenst = factory.getOWLClass(IRI.create(pre_SOSAOnt + "FeatureOfInterenst"));
+		
+		ontology.add(factory.getOWLDeclarationAxiom(System));
+		ontology.add(factory.getOWLDeclarationAxiom(Platform));
+		ontology.add(factory.getOWLSubClassOfAxiom(Sensor,System));
+		ontology.add(factory.getOWLDeclarationAxiom(Property));
+		ontology.add(factory.getOWLSubClassOfAxiom(ObservableProperty, Property));
+		ontology.add(factory.getOWLDeclarationAxiom(Observation));
+		ontology.add(factory.getOWLDeclarationAxiom(FeatureOfInterenst));
+		
+		OWLObjectProperty madeObservation = factory.getOWLObjectProperty(IRI.create(pre_SOSAOnt + "madeObservation"));
+		OWLObjectPropertyDomainAxiom domainAxiom = factory.getOWLObjectPropertyDomainAxiom(madeObservation, Sensor);
+		OWLObjectPropertyRangeAxiom rangeAxiom = factory.getOWLObjectPropertyRangeAxiom(madeObservation, Observation);
+		ontology.add(factory.getOWLDeclarationAxiom(madeObservation));
+		ontology.add(domainAxiom);
+		ontology.add(rangeAxiom);
+		
+		OWLObjectProperty observedProperty = factory.getOWLObjectProperty(IRI.create(pre_SOSAOnt + "observedProperty"));
+		OWLObjectPropertyDomainAxiom observedPropertydomainAxiom = factory.getOWLObjectPropertyDomainAxiom(observedProperty, Observation);
+		OWLObjectPropertyRangeAxiom observedPropertyrangeAxiom = factory.getOWLObjectPropertyRangeAxiom(observedProperty, ObservableProperty);
+		ontology.add(factory.getOWLDeclarationAxiom(observedProperty));
+		ontology.add(observedPropertydomainAxiom);
+		ontology.add(observedPropertyrangeAxiom);
+		
+		OWLObjectProperty hasFeatureOfInterest = factory.getOWLObjectProperty(IRI.create(pre_SOSAOnt + "hasFeatureOfInterest"));
+		OWLObjectPropertyDomainAxiom hasFeatureOfInterestdomainAxiom = factory.getOWLObjectPropertyDomainAxiom(hasFeatureOfInterest, Observation);
+		OWLObjectPropertyRangeAxiom hasFeatureOfInterestrangeAxiom = factory.getOWLObjectPropertyRangeAxiom(hasFeatureOfInterest, FeatureOfInterenst);
+		ontology.add(factory.getOWLDeclarationAxiom(observedProperty));
+		ontology.add(hasFeatureOfInterestdomainAxiom);
+		ontology.add(hasFeatureOfInterestrangeAxiom);
+		
+		OWLObjectProperty hosts = factory.getOWLObjectProperty(IRI.create(pre_SOSAOnt + "hosts"));
+		OWLObjectPropertyDomainAxiom hostsdomainAxiom = factory.getOWLObjectPropertyDomainAxiom(hosts, Platform);
+		OWLObjectPropertyRangeAxiom hostsrangeAxiom = factory.getOWLObjectPropertyRangeAxiom(hosts, Sensor);
+		ontology.add(factory.getOWLDeclarationAxiom(hosts));
+		ontology.add(hostsdomainAxiom);
+		ontology.add(hostsrangeAxiom);
+		
+		OWLObjectProperty hasProperty = factory.getOWLObjectProperty(IRI.create(pre_SSNOnt + "hasProperty"));
+		OWLObjectPropertyDomainAxiom hasPropertydomainAxiom = factory.getOWLObjectPropertyDomainAxiom(hasProperty, FeatureOfInterenst);
+		OWLObjectPropertyRangeAxiom hasPropertyrangeAxiom = factory.getOWLObjectPropertyRangeAxiom(hasProperty, Property);
+		ontology.add(factory.getOWLDeclarationAxiom(hasProperty));
+		ontology.add(hasPropertydomainAxiom);
+		ontology.add(hasPropertyrangeAxiom);
+		
+		OWLClass TemporalEntity = factory.getOWLClass(IRI.create(pre_TIME + "TemporalEntity"));
+		OWLClass Instant        = factory.getOWLClass(IRI.create(pre_TIME + "Instant"));
+		OWLClass Interval       = factory.getOWLClass(IRI.create(pre_TIME + "Interval"));
+		
+		ontology.add(factory.getOWLDeclarationAxiom(TemporalEntity));
+		ontology.add(factory.getOWLSubClassOfAxiom(Instant, TemporalEntity));
+		ontology.add(factory.getOWLSubClassOfAxiom(Interval, TemporalEntity));
+
+		OWLObjectProperty hasBeginning = factory.getOWLObjectProperty(IRI.create(pre_TIME + "hasBeginning"));
+		OWLObjectPropertyDomainAxiom hasBeginningdomainAxiom = factory.getOWLObjectPropertyDomainAxiom(hasBeginning, TemporalEntity);
+		OWLObjectPropertyRangeAxiom hasBeginningrangeAxiom = factory.getOWLObjectPropertyRangeAxiom(hasBeginning, Instant);
+		ontology.add(factory.getOWLDeclarationAxiom(hasBeginning));
+		ontology.add(hasBeginningdomainAxiom);
+		ontology.add(hasBeginningrangeAxiom);
+		
+		OWLObjectProperty hasEnd = factory.getOWLObjectProperty(IRI.create(pre_TIME + "hasEnd"));
+		OWLObjectPropertyDomainAxiom hasEnddomainAxiom = factory.getOWLObjectPropertyDomainAxiom(hasEnd, TemporalEntity);
+		OWLObjectPropertyRangeAxiom hasEndrangeAxiom = factory.getOWLObjectPropertyRangeAxiom(hasEnd, Instant);
+		ontology.add(factory.getOWLDeclarationAxiom(hasEnd));
+		ontology.add(hasEnddomainAxiom);
+		ontology.add(hasEndrangeAxiom);
+		
+		OWLDataProperty resultTime = factory.getOWLDataProperty(IRI.create(pre_SOSAOnt + "resultTime"));
+		ontology.add(factory.getOWLDeclarationAxiom(resultTime));
+		
+        //OntologyManager m = OntManagers.createONT();
+        //OWLDataFactory df = m.getOWLDataFactory();
+
+        // compose ontology:
+        //OntologyModel o = m.createOntology(IRI.create(uri));
+        //OWLAnnotationProperty a1 = df.getOWLAnnotationProperty(IRI.create(ns, "prop1"));
+        //OWLAnnotationProperty a2 = df.getOWLAnnotationProperty(IRI.create(ns, "prop2"));
+        //OWLClass c1 = df.getOWLClass(IRI.create(ns, "class1"));
+        //OWLNamedIndividual i1 = df.getOWLNamedIndividual(IRI.create(ns, "indi1"));
+        //o.add(df.getOWLDeclarationAxiom(a1));
+        //o.add(df.getOWLDeclarationAxiom(a2));
+        //o.add(df.getOWLDeclarationAxiom(c1));
+        //o.add(df.getOWLAnnotationPropertyDomainAxiom(a1, c1.getIRI()));
+        //o.add(df.getOWLClassAssertionAxiom(c1, i1));
+        //o.add(df.getOWLSubAnnotationPropertyOfAxiom(a2, a1));
+        //o.add(df.getOWLAnnotationAssertionAxiom(a1, df.getOWLAnonymousIndividual(), i1.getIRI()));
+
+        //String pre_TIME = "http://www.w3.org/2006/time#";
+		//OWLImportsDeclaration importDeclarationTIME = m.getOWLDataFactory().getOWLImportsDeclaration(IRI.create(pre_TIME));
+		//m.applyChange(new AddImport(o, importDeclarationTIME));
+		
+        
+        //Create a file for the new format
+        File fileformated = new File("/home/franco/Repositories/lab.ssn.ontology/test.owl");
+        
         try {
-			o.saveOntology(System.out);
+			//ontology.saveOntology(System.out);
+        	ontology.saveOntology(IRI.create(fileformated.toURI()));
 		} catch (OWLOntologyStorageException e1) {
 			e1.printStackTrace();
 		}
 
-        // SPARQL UPDATE: replace owl:AnnotationProperty -> owl:ObjectProperty
-        UpdateAction.parseExecute(
-                "DELETE { ?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/07/owl#AnnotationProperty> } " +
-                "INSERT { ?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/07/owl#ObjectProperty> } " +
-                "WHERE { ?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/07/owl#AnnotationProperty> }",
-                o.asGraphModel());
+    
 
-        // Print result:
-        //o.axioms().forEach(System.out::println);
-        try {
-			o.saveOntology(System.out);
-		} catch (OWLOntologyStorageException e) {
-			e.printStackTrace();
-		}
+	/* Populate ontology with data from sensors */
+	OntologyAssistant oa = new OntologyAssistant();
 
-        // SPARQL SELECT: just print all declarations:
-        try (QueryExecution qexec = QueryExecutionFactory.create(QueryFactory.create("SELECT ?x ?y WHERE { ?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?y }"), o.asGraphModel())) {
-            ResultSet res = qexec.execSelect();
-            while (res.hasNext()) {
-                //System.out.println(res.next());
-            }
-        }
+	//FileInputStream fstream = null;
+	//try {
+	//	fstream = new FileInputStream("/home/franco/DataSets/SECOM/secom_final.data");
+	//} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+	//	e.printStackTrace();
+	//}
+	//BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+	//String strLine;
+	//int i = 0;
+	//int cont = 0;
+	//try {
+	//while (((strLine = br.readLine()) != null) && (cont<10))   {
+	//	cont++;
+	//	i++;
+	//	OWLNamedIndividual O1 = factory.getOWLNamedIndividual(IRI.create(ns, "OBS1"));
+	//	String dString = strLine.split(" ")[1];
+	//	String newD = dString.split("-")[2] + "-" + dString.split("-")[1] + "-" + dString.split("-")[0];
+			
+			//OWLLiteral date = df.getOWLLiteral(newD + "T" + strLine.split(" ")[2]+".000",OWL2Datatype.XSD_DATE_TIME);
+		    //oa.createIndividual(o, m, df, contextOntIRI + "#TI" + Integer.toString(i), ValidInstant);
+		    //oa.assignValueToDataTypeProperty(ontology, manager, factory, hasTimeT, factory.getOWLNamedIndividual(IRI.create(contextOntIRI + "#TI" + Integer.toString(i))), date);
+		    //oa.createIndividual(ontology, manager, factory, contextOntIRI + "#O" + Integer.toString(i) , Observation);
+			//oa.relateIndividuals(ontology, manager, factory, madeBySensor, factory.getOWLNamedIndividual(IRI.create(contextOntIRI + "#O" + Integer.toString(i))), Sensor_1);
+			//oa.relateIndividuals(ontology, manager, factory, observedProperty, factory.getOWLNamedIndividual(IRI.create(contextOntIRI + "#O" + Integer.toString(i))), OProperty_1);
+			//oa.assignValueToDataTypeProperty(ontology, manager, factory, hasSimpleResult, factory.getOWLNamedIndividual(IRI.create(contextOntIRI + "#O" + Integer.toString(i))), factory.getOWLLiteral(Double.parseDouble(strLine.split(" ")[3])));
+			//oa.assignValueToDataTypeProperty(ontology, manager, factory, resultTime, factory.getOWLNamedIndividual(IRI.create(contextOntIRI + "#O" + Integer.toString(i))), date);
+			//oa.relateIndividuals(ontology, manager, factory, hasTimeObs, factory.getOWLNamedIndividual(IRI.create(contextOntIRI + "#O" + Integer.toString(i))), factory.getOWLNamedIndividual(IRI.create(contextOntIRI + "#TI" + Integer.toString(i)))); 
+
+	//		System.out.println (newD+ "T" + strLine.split(" ")[2]);
+				
+				//SWRLRuleEngine ruleEngine = SWRLAPIFactory.createSWRLRuleEngine(ontology);   			
+				//ruleEngine.infer();
+
+				//ruleEngine.importAssertedOWLAxioms();
+				//ruleEngine.run();
+				//ruleEngine.exportInferredOWLAxioms();
+	//}
+	//} catch (IOException e) {
+		// TODO Auto-generated catch block
+	//e.printStackTrace();
+	//}
+	//try {
+	//	br.close();
+	//} catch (IOException e) {
+		// TODO Auto-generated catch block
+	//e.printStackTrace();
+	//}
     }
 }
